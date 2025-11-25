6ea67a64294ac3486db8f7b95500d2892cc20a69
hyeonmi <hyeonmi.kim@sk.com>
2025-11-25 23:41:44 +0900

    reducer 파일 분리

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -3,24 +3,10 @@ import { useRef, useReducer } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
-      return state.map((todo) =>
-        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
-      );
-    case "DELETE":
-      return state.filter((todo) => todo.id !== action.targetId);
-    default:
-      return state;
-  }
-}
+import { initialState, reducer } from "./todosReducer";
 
 function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
+  const [todos, dispatch] = useReducer(reducer, initialState);
   const idRef = useRef(0);
 
   const onCreate = (content) => {
