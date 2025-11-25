6be226aabc6a65ca23f56a07b6b98b2eeed17b23
hyeonmi <hyeonmi.kim@sk.com>
2025-11-25 23:38:30 +0900

    add reducer

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,38 +1,52 @@
 import "./App.css";
-import { useState, useRef } from "react";
+import { useRef, useReducer } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([newTodo, ...todos]);
-  };
-
-  const onDelete = (id) => {
-    const resultTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(resultTodos);
-  };
-
-  const updateDone = (id) => {
-    const updateTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isDone: !todo.isDone };
-      } else {
-        return todo;
-      }
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
     });
-    setTodos(updateTodos);
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
+
+  const updateDone = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   return (
