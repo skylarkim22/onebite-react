889e5381f4bec4a788549d8852b62068af7812b1
hyeonmi <hyeonmi.kim@sk.com>
2025-11-26 20:57:16 +0900

    동적 라우팅

--- section13/src/components/Layout.jsx
+++ section13/src/components/Layout.jsx
@@ -1,7 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Layout = ({ children }) => {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/");
+  };
   return (
     <div>
-      layout
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <button onClick={onClickButton}>go home</button>
       <div>{children}</div>
     </div>
   );
--- section13/src/pages/Diary.jsx
+++ section13/src/pages/Diary.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams();
+
+  return <div>{params.id}번 Diary</div>;
 };
 
 export default Diary;
