77f6e2f698c689fcdb262ef0328c9c7a54b7c47b
hyeonmi <hyeonmi.kim@sk.com>
2025-11-26 20:41:20 +0900

    react router 적용

---
+++ section13/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ section13/src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+  return <div>잘못된 접근입니다.</div>;
+};
+
+export default Notfound;
