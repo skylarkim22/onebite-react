a17746e44810a0bebbfe30c99603183333247534
hyeonmi <hyeonmi.kim@sk.com>
2025-11-27 14:36:19 +0900

    Edit 페이지 추가

--- section13/src/App.jsx
+++ section13/src/App.jsx
@@ -4,45 +4,20 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import Edit from "./pages/Edit";
 import Button from "./components/Button";
 import Header from "./components/Header";
+
 import { getEmotionImage } from "./util/get-emotion-image";
 
 function App() {
   return (
     <>
-      <Header
-        title="제목"
-        rightChild={<Button text="right" />}
-        leftChild={<Button text="left" />}
-      />
-      <Button
-        text="123"
-        type="DEFAULT"
-        onClick={() => {
-          console.log("123 click");
-        }}
-      />
-      <Button
-        text="123"
-        type="NEGATIVE"
-        onClick={() => {
-          console.log("123 click");
-        }}
-      />
-
-      <Button
-        text="123"
-        type="POSITIVE"
-        onClick={() => {
-          console.log("123 click");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
