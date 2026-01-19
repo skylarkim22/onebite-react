054964de95a73a5d2769140403cc6a1f6a13a28e
hyeonmi <hyeonmi.kim@sk.com>
2026-01-19 16:56:50 +0900

    fix: memo 사용한 리렌더링 방지

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ todo, onDelete, updateDone }) => {
   const onClickButton = () => {
@@ -18,5 +19,10 @@ const TodoItem = ({ todo, onDelete, updateDone }) => {
     </div>
   );
 };
-
-export default TodoItem;
+// memo 메서드는 얕은 비교를 하기때문에 onDelete, updateDone이 리렌더링될때마다 해당 함수가 새로 생성되기때문에 변경됐다고 판단함
+// 두번재 인자에 콜백함수를 체크하는 로직을 넣을수가 있다.
+export default memo(TodoItem, (preProps, nextProps) => {
+  // true -> props 바꾸지 않은, false -> props 바뀜
+  if (preProps.todo !== nextProps.todo) return false;
+  return true;
+});
